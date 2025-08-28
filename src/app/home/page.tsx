de9bfafe3a561d798c04c91880837a5912af6bb0
gurjaspartap <gurjas082@gmail.com>
2025-08-29 00:13:18 +1000

    initial commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to Cloud Sync Project</h1>
+      <p>This is the home page of your Next.js app.</p>
+    </main>
+  );
+};
+
+export default HomePage;
